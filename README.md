7973a5124937f720c056c187ddb47d5a62e9c8a0
Mork <mawwwrk@gmail.com>
2022-01-11 13:32:59 +0000

    commit ts and compiled js code for comparison

--- README.md
+++
@@ -1,7 +0,0 @@
-# gameproject
-repo for SEI project 1
-
-## Todos
-- create canvas and movement system
-- collision detection system
-- spritesheet animation
--- src/types/types.ts
+++
@@ -1,3 +0,0 @@
-export type CanvasAndWindow = { canvas: HTMLCanvasElement, window: Window }
-export enum Axis { Vertical, Horizontal }
-export type Dimensions = { height: number, width: number }
