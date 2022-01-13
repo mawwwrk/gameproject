ccab48e55ee60e75336e303239efcaf4c89e2f4f
Mork <mawwwrk@gmail.com>
2022-01-13 15:30:22 +0800

    restructured project for JS

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import "https://cdn.skypack.dev/normalizecss";
+import "./style.css";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+/** @type {HTMLDivElement} */ //@ts-ignore
+const app = document.getElementById("app");
+
+// app.innerHTML = "<h1>hello</h1>";
+app;
