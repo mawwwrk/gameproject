414d560fa9e4a9284ace9fd2eefc1d221e3d9598
Mork <mawwwrk@gmail.com>
2022-01-19 01:40:23 +0800

    contain function to keep sprites in bounds

--- src/components/stage.js
+++ src/components/stage.js
@@ -2,39 +2,58 @@
  * @param {import("../classes").DisplayObject} contain
  */
 export function contain(sprite, bounds, bounce = false) {
-  let x = bounds.x;
-  let y = bounds.y;
-  let width = bounds.width;
-  let height = bounds.height;
+  let x = bounds.x,
+    y = bounds.y,
+    width = bounds.width,
+    height = bounds.height;
   let collision;
   let bounciness = 0.96;
 
+  let target = {};
+  if (sprite.hitbox) {
+    target.x = sprite.hitbox.gx;
+    target.y = sprite.hitbox.gy;
+    target.width = sprite.hitbox.width;
+    target.height = sprite.hitbox.height;
+  } else {
+    target.x = sprite.x;
+    target.y = sprite.y;
+    target.width = sprite.width;
+    target.height = sprite.height;
+  }
+
+  let offsetWidth = sprite.hitbox
+    ? (sprite.width - sprite.hitbox.width) / 2
+    : 0;
+  let offsetHeight = sprite.hitbox
+    ? (sprite.height - sprite.hitbox.height) / 2
+    : 0;
   //* left
-  if (sprite.x < 0) {
+  if (target.x < 0) {
     if (bounce) sprite.vx *= -bounciness;
     if (sprite.mass) sprite.vx /= sprite.mass;
-    sprite.x = x;
+    sprite.x = x - offsetWidth;
     collision = "left";
   }
   //* top
-  if (sprite.y < y) {
+  if (target.y < y) {
     if (bounce) sprite.vy *= -bounciness;
     if (sprite.mass) sprite.vy /= sprite.mass;
-    sprite.y = y;
+    sprite.y = y - offsetHeight;
     collision = "top";
   }
   //* right
-  if (sprite.x + sprite.width > width) {
-    if (bounce) sprite.vx *= -bounciness;
+  if (target.x + target.width > width) {
+    if (bounce) target.vx *= -bounciness;
     if (sprite.mass) sprite.vx /= sprite.mass;
-    sprite.x = width - sprite.width;
+    sprite.x = width - sprite.width + offsetWidth;
     collision = "right";
   }
   //* bottom
-  if (sprite.y + sprite.height > height) {
+  if (target.y + target.height > height) {
     if (bounce) sprite.vy *= -bounciness;
     if (sprite.mass) sprite.vy /= sprite.mass;
-    sprite.y = height - sprite.height;
+    sprite.y = height - sprite.height + offsetHeight;
     collision = "bottom";
   }
 
